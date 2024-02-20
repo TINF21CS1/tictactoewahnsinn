ef2f7f41b1c63fc8ba4b7fc78bdac69d6d592af8
j0sh1438 <github.tuhys@simplelogin.co>
2024-02-20 19:24:45 +0100

    feat: add basic functionality
    Introduced the classes Player and Opponent. User inherits from Player. Changed class names to CapWords convention. Added getter methods and the possiblity to change name and delete profile of User.

--- user.py
+++ user.py
@@ -1,15 +1,52 @@
 import json
 import uuid
+from abc import ABC, abstractmethod
+import os
 
-class user():
+class Player():
     def __init__(self, name: str, wins=0, draws=0, losses=0, id=uuid.uuid4()):
         self._name = name
         self._id = uuid.uuid4()
         self._wins = wins 
         self._draws = draws 
         self._losses = losses
-        self.save_data()
+    
+    @abstractmethod
+    def make_move(self):
+        pass
+
+    def get_id(self):
+        return self._id
+
+    def get_name(self):
+        return self._name
+
+    def get_wins(self):
+        return self._wins
+
+    def get_draws(self):
+        return self._draws
+
+    def get_losses(self):
+        return self._losses
+
+    def __str__(self):
+        return f"{self._name} {self._id} {self._wins} {self._draws} {self._losses}"
+
+    def _updateStatistics(self, status: str): 
+        match status:
+            case "win":
+                self._wins+=1
+            case "draw":
+                self._draws+=1
+            case "loss":
+                self._losses+=1
+        self._save_data()
+
+    
+
         
+class User(Player):
     @classmethod
     def from_data(cls): 
         with open('user_data.json', 'r') as user_data:
@@ -21,33 +58,31 @@ class user():
         losses = data["losses"]
 
         return cls(name, wins, draws, losses, id)
-    
-    def __str__(self):
-        return f"{self._name} {self._id} {self._wins} {self._draws} {self._losses}"
 
-    def updateStatistics(self, status: str): 
-        match status:
-            case "win":
-                self._wins+=1
-            case "draw":
-                self._draws+=1
-            case "loss":
-                self._losses+=1
-        self.save_data()
-
-    def save_data(self):
+    def _save_data(self):
         with open("user_data.json", "w") as user_data:
             json.dump({"name": self._name, "id": str(self._id), "wins": self._wins, "draws": self._draws, "losses": self._losses}, user_data) 
 
+    def change_name(self, name):
+        self._name = name
+        self._save_data()
 
-    
+    def make_move(self):
+        pass
 
-# x = user("test")
-x = user.from_data()
+    def delete_profile(self):
+        os.remove("user_data.json")
+
+class Opponent(Player):
+    pass
+
+
+x = User("hallo")
 print(x)
-x.updateStatistics("loss")
+x._updateStatistics("loss")
 print(x)
-
-            
-    
-
+y = Opponent("gegner")
+x.change_name("absoluter boss")   
+print(x)
+x.delete_profile()
+print(x)
