1b50d1b6fb94fe913f5f41353131db8c8c959eb2
j0sh1438 <github.tuhys@simplelogin.co>
2024-02-20 18:10:39 +0100

    feat: add save functionality
    This adds the possibility to save userdata in a json file. A user object can also be created using the contents of such a file.

--- user.py
+++ user.py
@@ -2,27 +2,50 @@ import json
 import uuid
 
 class user():
-    def __init__(self, name: str):
+    def __init__(self, name: str, wins=0, draws=0, losses=0, id=uuid.uuid4()):
         self._name = name
         self._id = uuid.uuid4()
-        self._wins = 0
-        self._draws = 0
-        self._losses = 0
-
+        self._wins = wins 
+        self._draws = draws 
+        self._losses = losses
+        self.save_data()
+        
     @classmethod
-    def user_from_data(cls, data): 
+    def from_data(cls): 
         with open('user_data.json', 'r') as user_data:
-            data=user_data.read()
+            data=json.load(user_data)
+        name = data["name"]
+        id = data["id"]
+        wins = data["wins"]
+        draws = data["draws"]
+        losses = data["losses"]
+
+        return cls(name, wins, draws, losses, id)
     
     def __str__(self):
         return f"{self._name} {self._id} {self._wins} {self._draws} {self._losses}"
 
-    def updateStatistics(self): 
-        pass
+    def updateStatistics(self, status: str): 
+        match status:
+            case "win":
+                self._wins+=1
+            case "draw":
+                self._draws+=1
+            case "loss":
+                self._losses+=1
+        self.save_data()
+
+    def save_data(self):
+        with open("user_data.json", "w") as user_data:
+            json.dump({"name": self._name, "id": str(self._id), "wins": self._wins, "draws": self._draws, "losses": self._losses}, user_data) 
 
 
+    
 
-x = user("test")
+# x = user("test")
+x = user.from_data()
+print(x)
+x.updateStatistics("loss")
 print(x)
 
             
